d50525463f8084dbe233d747d6694002e2761e5b
MariusHull <marius.hullin@gmail.com>
2019-04-15 23:08:00 +0100

    update prod

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 
 // Security options.
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://monithor.fr");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
